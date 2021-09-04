903b794e8d1abd8e59ee626bcc321c13cc0ac313
junhyuck <63086954+YangJunHyuck@users.noreply.github.com>
2021-09-04 14:36:21 +0900

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -8,25 +8,21 @@ result = []
 def newsurl():
     # 나스닥 주요 주식들 리스트
     news_keyword = ["Apple", "microsoft", "Amazon", "facebook", "tesla", "NVIDIA", "PayPal", "Intel", "Netflix",
-                    "Adobe",
-                    "AlphabetInc", "ASMLHolding", "ComcastCorporation", "Broadcom"]
-
+                    "Adobe", "Alphabet+Inc", "ASML+Holding", "Comcast+Corporation", "Broadcom"]
     return news_keyword
 
 
-def Community_word():
-    commnity_keyword = ["Apple+Stock", "microsoft+Stock", "Amazon+Stock", "facebook+Stock", "tesla+Stock",
-                        "NVIDIA+Stock", "PayPal+Stock", "Intel+Stock", "Netflix+Stock", "Adobe+Stock"]
-
-    return commnity_keyword
-
-
 def newscrawler():
+
+
     news_keyword = newsurl()
+    print(len(news_keyword))
     # 구글 뉴스url
     url = "https://news.google.com"
     # 나스닥 링크 필요시에 이것만 변경해서 원하는 종목 크롤링 가능
-    for i in range(0, 12):
+
+    for i in range(-1,12):
+        print(i)
         news_url = url + "/search?q=" + news_keyword[i] + "&hl=en-US&gl=US&ceid=US%3Aen"
 
         stocknews = requests.get(news_url)
@@ -44,7 +40,9 @@ def newscrawler():
             news_title = article.find('a', attrs={'class': 'DY5T1d'}).getText()
 
             news_report = article.find('time', attrs={'class': 'WW6dff uQIVzc Sksgp'})
-            news_datetime = news_report.get('datetime').split('T')
+
+            news_datetime = news_report.get('datetime').split("T")
+
 
             news_date = news_datetime[0][:-1]
             news_time = news_datetime[1][:-1]
@@ -56,41 +54,13 @@ def newscrawler():
     return result
 
 
-# def community_crawler():
-#     commnity_keyword = community_crawler()
-#     # 레딧 뉴스url
-#     url = "https://www.reddit.com/"
-#     # 링크 필요시에 이것만 변경해서 원하는 종목 크롤링 가능
-#     for i in range(0, 9):
-#         news_url = url + "/search?q=" + commnity_keyword[i]
-#
-#         stockcom = requests.get(news_url)
-#         html_src = stockcom.text
-#         soup = BeautifulSoup(html_src, 'html.parser')
-#         # div xrnccd 속성 부분 추출
-#         news = soup.select('div[class="_1Y6dfr4zLlrygH-FLmr8x- "]')
-#
-#         # 현재 크롤링한 뉴스 개수
-#
-#         print(len(news))
-#
-#         for article in news:
-#             # 제목 a태그에 SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE속성
-#             cmnews_title = article.find('a', attrs={'class': 'SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE'}).getText()
-#
-#             # rseult 라는 리스트에 날짜 시간 제목 이란 데이터 넣기
-#             result.append(cmnews_title)
-#
-#     return result
-
-
 n = 3
 
 
 def split_list(l, n):
     # 리스트 l의 길이가 n이면 계속 반복
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+    for o in range(0, len(l), n):
+        yield l[o:o + n]
 
 
 # 리스트 값들 3개를 한묶음으로 날짜,시간,제목
