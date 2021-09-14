0089ece696da793bd68a9439b05773e7e22dd354
junhyuck <63086954+YangJunHyuck@users.noreply.github.com>
2021-09-14 20:57:16 +0900

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+import string
 result = []
 
 
@@ -14,7 +14,6 @@ def newsurl():
 
 def newscrawler():
 
-
     news_keyword = newsurl()
     print(len(news_keyword))
     # 구글 뉴스url
@@ -22,13 +21,13 @@ def newscrawler():
     # 나스닥 링크 필요시에 이것만 변경해서 원하는 종목 크롤링 가능
 
     for i in range(-1,12):
-        print(i)
         news_url = url + "/search?q=" + news_keyword[i] + "&hl=en-US&gl=US&ceid=US%3Aen"
 
         stocknews = requests.get(news_url)
         html_src = stocknews.text
         soup = BeautifulSoup(html_src, 'html.parser')
         # div xrnccd 속성 부분 추출
+
         news = soup.select('div[class="xrnccd"]')
 
         # 현재 크롤링한 뉴스 개수
@@ -37,24 +36,36 @@ def newscrawler():
 
         for article in news:
             # 제목 a태그에 DY5T1d속성
+
+
             news_title = article.find('a', attrs={'class': 'DY5T1d'}).getText()
 
             news_report = article.find('time', attrs={'class': 'WW6dff uQIVzc Sksgp'})
 
             news_datetime = news_report.get('datetime').split("T")
 
+            news_link = article.find('a',attrs={'class':'VDXfz'}).get('href')
+
+
 
             news_date = news_datetime[0][:-1]
             news_time = news_datetime[1][:-1]
+
             # rseult 라는 리스트에 날짜 시간 제목 이란 데이터 넣기
+
             result.append(news_date)
             result.append(news_time)
             result.append(news_title)
+            result.append(news_link)
+
+
+
 
     return result
 
 
-n = 3
+n = 4
+
 
 
 def split_list(l, n):
@@ -63,11 +74,15 @@ def split_list(l, n):
         yield l[o:o + n]
 
 
-# 리스트 값들 3개를 한묶음으로 날짜,시간,제목
+# 리스트 값들 4개를 한묶음으로 날짜,시간,제목
+
 result = split_list(newscrawler(), n)
 
+
 # 크롤링한 데이터 데이터 프레임으로 만들기
-df = pd.DataFrame.from_records(result, columns=['date', 'time', 'title'])
+df = pd.DataFrame.from_records(result, columns=['date', 'time', 'title','link'])
+
+
 
 df = df.sort_values(by=['date', 'time'], ignore_index=True)
 # 엑셀파일로 크롤링한 데이터 저장
